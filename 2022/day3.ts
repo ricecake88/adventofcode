46a210838165738e2b3cbee9010a1a66b069fc26
Grace Shih <gracelearnsforever@gmail.com>
2023-08-31 22:32:09 -0700

    Added code for day3; can pass and group whatever number

--- 2022/day3.ts
+++ 2022/day3.ts
@@ -13,17 +13,19 @@ function calcPriority(char: string): number {
     return 0;
 }
 
+/* returns an array of unique values*/
 function compareTwoRucksacks(rucksack1: string[], rucksack2: string[]) {
-    let val;
+    let val = [];
     for (let i = 0; i < rucksack1.length; i++) {
         if (rucksack2.includes(rucksack1[i])) {
-            val = rucksack1[i];
-            break;
+            val.push(rucksack1[i]);
+            
         }
     }
     return val;
 }
 
+/* return only the unique values of the rucksacks */
 function onlyUniques(rucksack: string) {
     return rucksack.split("").reduce((clean: string[], currValue: string) => {
         if (!clean.includes(currValue)) {
@@ -46,13 +48,55 @@ export function splitIntoBuckets(unprocessed: string) {
 
 }
 
-export function main() {
-    const lines = readFileSync('./tests/day3/day3-full-input.txt').toString().replace(/\r\n/g, " ").split(' ');
+export function partOne(lines: []) {
     let sum = 0;
     lines.forEach(line => {
         const [ bucket1, bucket2 ] = splitIntoBuckets(line);
         let commonItem = compareTwoRucksacks(bucket1, bucket2);
-        sum += commonItem ? calcPriority(commonItem) : 0;
+        sum += commonItem[0] ? calcPriority(commonItem[0]) : 0;
     })
     return sum;
+}
+
+
+/* calculate the sum based on common items grouped by numOfLines
+* parameters:
+*   lines - array of lines processed from input
+*   numOfLines = number of lines to group together to find the common item between
+*                the rucksacks
+*/
+export function partTwo(lines: [], numOfLines: number) {
+    let sum = 0;
+    for (let i = 0; i < lines.length; i++) {
+        const groupedLines = [];
+
+        // get all the lines to find same item
+        for (let j = 0; j < numOfLines; j++) {
+            groupedLines.push(onlyUniques(lines[i+j]));
+        }
+
+        // compare the lines repeatedly to find the common item
+        let lineNum = 0;
+        let commonItems = groupedLines[lineNum];
+        for (let lineNum = 0; lineNum < numOfLines-1; lineNum++) {
+           commonItems = compareTwoRucksacks(commonItems, groupedLines[lineNum+1]);
+        }
+
+        // calculate the priority of each commonly found item
+        sum += calcPriority(commonItems[0]);
+
+        // set counter as offset from number of lines to compare
+        i += numOfLines-1;
+    }
+    return sum;
+}
+
+export function main() {
+    let result = 0;
+    // part one calculations
+    // const lines = readFileSync('./tests/day3/day3-full-input.txt').toString().replace(/\r\n/g, " ").split(' ');
+    // result = partOne(lines as []);
+    const part2lines = readFileSync('./tests/day3/day3-full-input.txt').toString().replace(/\r\n/g, " ").split(' ');
+    result = partTwo(part2lines as [], 3);
+    return result;
 }
